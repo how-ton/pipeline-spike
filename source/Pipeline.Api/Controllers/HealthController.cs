fcd3172ff190d8fddea4bc76485d302af5fc1b88
MattyR <matt.rushton@dominos.co.uk>
2020-10-28 08:54:04 +0000

    Initial Commit

---
+++ source/Pipeline.Api/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pipeline.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public WeatherForecastController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
